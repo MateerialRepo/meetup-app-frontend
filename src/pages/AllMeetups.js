709008a0e947b8b9111c2478e1af57882ddc885e
MateerialRepo <belloolaluqman@gmail.com>
2023-01-23 01:09:05 +0000

    Added wrapper layout and more Ui component.Each component focused on a single task

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,7 +1,21 @@
 import React from "react";
+import MeetupList from "../components/meetups/MeetupList";
+
+const Dummy_data = [
+  {
+    id: 1,
+    title: "first Item",
+    image:
+      "http://img.izismile.com/img/img7/20141206/640/madonnas_latest_raunchy_photo_shoot_640_11.jpg",
+    description: "lorem Ipsum",
+  },
+];
 
 const AllMeetupsPage = () => {
-  return <section>All Meetups Page</section>;
+  return <section>
+    <h1>All Meetups</h1>
+    <MeetupList meetups = {Dummy_data} />
+  </section>;
 };
 
 export default AllMeetupsPage;
