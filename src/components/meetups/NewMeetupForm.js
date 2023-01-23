c69e34fec92d8e4cc1d46c2d9a4fd090cdc3f937
MateerialRepo <belloolaluqman@gmail.com>
2023-01-23 02:48:08 +0000

    Added post functionality to form for firebase

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -1,36 +1,68 @@
-import classes from './NewMeetupForm.module.css'
-import Card from '../ui/Card';
+import { useRef } from "react";
+import classes from "./NewMeetupForm.module.css";
+import Card from "../ui/Card";
 
-const NewMeetupForm = () => {
+const NewMeetupForm = (props) => {
+
+    const titleInput = useRef();
+    const imageInput = useRef();
+    const addressInput = useRef();
+    const descriptionInput = useRef();
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const enteredTitle = titleInput.current.value;
+        const enteredImage = imageInput.current.value;
+        const enteredAddress = addressInput.current.value;
+        const enteredDescription = descriptionInput.current.value;
+
+        const meetupData = {
+        title: enteredTitle,
+        image: enteredImage,
+        address: enteredAddress,
+        description: enteredDescription,
+        };
+
+        console.log(meetupData);
+
+        props.onAddMeetup(meetupData)
+  };
+
   return (
     <Card>
-      <form action="" className={classes.form}>
+      <form onSubmit={submitHandler} className={classes.form}>
         <div className={classes.control}>
           <label htmlFor="title">Meetup Title</label>
-          <input type="text" required id="title" />
+          <input ref={titleInput} type="text" required id="title" />
         </div>
 
         <div className={classes.control}>
-          <label htmlFor="image">Meetup Image</label>
-          <input type="url" required id="image" />
+          <label htmlFor="image">Meetup Image URL</label>
+          <input ref={imageInput} type="url" required id="image" />
         </div>
 
         <div className={classes.control}>
           <label htmlFor="address">Address</label>
-          <input type="text" required id="address" />
+          <input ref={addressInput} type="text" required id="address" />
         </div>
 
         <div className={classes.control}>
           <label htmlFor="description">Description</label>
-          <textarea id="description" rows="5" required></textarea>
+          <textarea
+            id="description"
+            rows="5"
+            ref={descriptionInput}
+            required
+          ></textarea>
         </div>
 
         <div className={classes.actions}>
-            <button>Add Meetup</button>
+          <button>Add Meetup</button>
         </div>
       </form>
     </Card>
   );
-}
+};
 
-export default NewMeetupForm
+export default NewMeetupForm;
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,11 +1,29 @@
-import React from "react";
+import {useHistory} from "react-router-dom";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 const NewMeetupPage = () => {
-  return <div>
-    <h1>Add New Meetup</h1>
-    <NewMeetupForm />
-  </div>;
+
+  const history = useHistory()
+
+  const addMeetupHandler = (meetupData) => {
+    fetch("https://meetup-app-58a46-default-rtdb.firebaseio.com/meetups.json", {
+      method: "POST",
+      body: JSON.stringify(meetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  };
+
+
+
+  return (
+    <div>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </div>
+  );
 };
+
 
 export default NewMeetupPage;
